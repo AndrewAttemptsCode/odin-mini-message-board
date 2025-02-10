74884d44f9af34153c4105509f99433922c93131
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-10 16:52:18 +0000

    feat: add details route with dynamic index

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { indexPage, newMessage, submitMessage } = require('../controllers/indexPage');
+const { indexPage, newMessage, submitMessage, getDetails } = require('../controllers/indexPage');
 
 const indexRouter = Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/new', newMessage);
 indexRouter.post('/new', submitMessage);
+indexRouter.get('/details/:index', getDetails);
 
 module.exports = indexRouter;
