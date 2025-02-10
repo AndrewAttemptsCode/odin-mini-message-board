56472fd180bbe5540c6cf9ea5374ec3586f6df2d
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-10 12:19:55 +0000

    feat: add sample messages and pass to the locals object

--- src/controllers/homePage.js
+++ src/controllers/homePage.js
@@ -1,5 +1,23 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amanda",
+    added: new Date().toLocaleString()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().toLocaleString()
+  },
+  {
+    text: "What's up!",
+    user: "James",
+    added: new Date().toLocaleString()
+  },
+];
+
 const homePage = (req, res) => {
-  res.render('index', { title: 'Mini Message Board' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 };
 
 module.exports = { homePage };
