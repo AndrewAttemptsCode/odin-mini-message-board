1646201b507e8d9f815304b6b01069912183824a
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-10 12:53:39 +0000

    feat: add route to submit a new message to the board

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const { indexPage } = require('../controllers/indexPage');
+const { newMessage } = require('../controllers/newMessage');
 
 const indexRouter = Router();
 
 indexRouter.get('/', indexPage);
+indexRouter.get('/new', newMessage);
 
 module.exports = indexRouter;
