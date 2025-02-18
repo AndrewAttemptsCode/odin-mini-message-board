f95ad6b16c8047360dc317f175991e5b8f12bafb
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-18 13:16:44 +0000

    feat: create function to query and return all messages

--- db/queries.js
+++ db/queries.js
@@ -1,2 +1,8 @@
 const pool = require('./pool');
 
+const allMessagesGet = async () => {
+  const { rows } = await pool.query('SELECT * FROM messages;');
+  return rows;
+};
+
+module.exports = { allMessagesGet };
