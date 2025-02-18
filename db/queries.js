4f92a702d223651e0323015b522ae67bfc863958
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-18 14:15:28 +0000

    refactor: changed to order by id in descending order so new messages get placed at the top

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const pool = require('./pool');
 
 const allMessagesGet = async () => {
-  const { rows } = await pool.query('SELECT * FROM messages;');
+  const { rows } = await pool.query('SELECT * FROM messages ORDER BY id DESC;');
   return rows;
 };
 
